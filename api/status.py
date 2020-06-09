c1ea20560050477c524330d696dbee00389c33ab
Fernando França (フランサ) <github@furansa.me>
2020-06-09 19:23:35 -0300

    Added standalone health checker.

--- api/status.py
+++ api/status.py
@@ -3,13 +3,12 @@ from flask import abort, jsonify, Response
 
 # Example set of status
 STATUS = {
-    "Version": "1.0.0",
-    "Status": "healthy",
+    "version": "1.0.0",
+    "status": "healthy",
 }
 
 
 def read_all() -> Response:
-    # TODO: Write unit test case (pytest)
     """
     Read and returns all health status, supports HTTP GET.
 
---
+++ api/utils/health_checker.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#
+# @file      health_checker.py
+# @version   0.0.1
+# @brief     Check application's health without using 3rd external tools
+#
+# ------------------------------------------------------------------------------
+#
+import json
+import urllib.request
+import sys
+
+
+def check_service_status(url: str) -> int:
+    """
+    Check general service status.
+
+    :param url: Service URL address
+    :type url: str
+
+    :return: 0 if healthy, otherwise 1
+    :rtype: int
+    """
+    with urllib.request.urlopen(url, timeout = 5) as response:
+        http_response_code = response.getcode()
+        http_response_data = json.loads(response.read().decode("utf8"))
+        service_status = http_response_data["status"]
+
+        if http_response_code == 200 and service_status == "healthy":
+            sys.exit(0)
+
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    """
+    Main program entrypoint
+    """
+    url = "http://localhost:8000/v1/status"
+
+    check_service_status(url)
+
+    sys.exit(1)

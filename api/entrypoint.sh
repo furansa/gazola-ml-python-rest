499dd80cda21cdf27c513c1af3dbcbdc6d2ccca0
Fernando França (フランサ) <github@furansa.me>
2020-06-09 14:20:52 -0300

    Starting Docker basic structure.

---
+++ api/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# @file      entrypoint.sh
+# @version   0.0.1
+# @brief     Docker entrypoint script
+#
+# ------------------------------------------------------------------------------
+#
+# Leave if any command fails
+set -e
+
+# Used for debugging if no foreground application is being called
+while true; do
+   date
+   uptime
+   sleep 5
+done
+
+# Call the application in foreground
+# /usr/bin/env python3 gazola.py
